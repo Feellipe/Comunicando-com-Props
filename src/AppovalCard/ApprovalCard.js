9892d75e9be11c19d4afb5dde528411c41a1aea6
Felipe Cavalcanti <fel.cavalcanti@hotmail.com>
2018-11-29 19:57:36 -0200

    Update

--- src/AppovalCard/ApprovalCard.js
+++ src/AppovalCard/ApprovalCard.js
@@ -1,25 +1,68 @@
 import React from 'react';
 import Button from './Button';
 import Comments from '../Comment';
+import styled from 'styled-components';
 
 
-const ApprovalCard = ({author, time, post}) => (
-    <div className="ui cards" >
-        <div className="content">
-            <Comments
-                author={author}
-                time={time}
-                post={post}
-            />
-        </div>
+const ApprovalCard = props => (
+    <Wrapper >
+        <Content >
+            {props.children}
+        </Content>
   
-        <div className="extra content">
+        <ExtraContent >
             <div className="ui two buttons">
                 <Button></Button>     
             </div>
-        </div>
-    </div>
+        </ExtraContent>
+    </Wrapper>
 );
+
+const ExtraContent = styled.div`
+    max-width: 100%;
+    min-height: 0!important;
+    -webkit-box-flex: 0;
+    -ms-flex-positive: 0;
+    flex-grow: 0;
+    border-top: 1px solid rgba(0,0,0,.05)!important;
+    position: static;
+    background: 0 0;
+    width: auto;
+    margin: 0 0;
+    padding: .75em 1em;
+    top: 0;
+    left: 0;
+    color: rgba(0,0,0,.4);
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    -webkit-transition: color .1s ease;
+    transition: color .1s ease;
+
+`;
+
+const Content = styled.div`
+    flex-grow: 1;
+    border: none;
+    /* border-top: 1px solid rgba(34,36,38,.1); */
+    background: 0 0;
+    margin: 0;
+    padding: 1em 1em;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    font-size: 1em;
+`;
+
+const Wrapper = styled.div`
+    display:flex;
+    flex-wrap: wrap;
+    margin: 10px;
+    border-radius: 3px;
+    border: 1px solid lightgrey;
+    flex-direction: column;
+    max-width: 280px;
+    padding: 3px;
+
+`;
 
 
 export default ApprovalCard;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import ApprovalCard from './AppovalCard/ApprovalCard';
 import styled from 'styled-components';
+import Comments from './Comment';
+
 
 const App = () => {
     return (
         <Wrapper>
-        <div className="ui container comments">
-            <ApprovalCard
-                author="Florzinha"
-                time="Tomorrow 5:59 PM"
-                post="Subarashi"
-            />            
-        </div>
+            <div className="ui container comments">       
+
+            <ApprovalCard>
+                <Comments
+                    author="Florzinha"
+                    time="Tomorrow 5:59 PM"
+                    post="Subarashi"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comments
+                    author="Lindinha"
+                    time="Today 8:00 AM"
+                    post="Omawa"
+                    />
+            </ApprovalCard>
+
+            <ApprovalCard>
+            <Comments
+                author="Docinho"
+                time="Today 8:33PM"
+                post="Nani?!"
+            />
+            </ApprovalCard>
+            </div>
         </Wrapper>
     )
 }
@@ -20,21 +40,24 @@ const App = () => {
 const Wrapper = styled.div`
     margin: 20px 0 0; 
 `;
-// <Comments
-// author="Florzinha"
-// time="Tomorrow 5:59 PM"
-// post="Subarashi"
-// />
-// <Comments
-// author="Lindinha"
-// time="Today 8:00 AM"
-// post="Omawa"
-// />
-// <Comments
-// author="Docinho"
-// time="Today 8:33PM"
-// post="Nani?!"
-// />
+
+{/* <ApprovalCard
+author="Florzinha"
+time="Tomorrow 5:59 PM"
+post="Subarashi"
+/>
+
+<ApprovalCard
+author="Florzinha"
+time="Today 8:00 AM"
+post="Omawa"
+/>     
+
+<ApprovalCard
+author="Florzinha"
+time="Tomorrow 8:33 PM"
+post="Nani"
+/>   */}
 
 ReactDOM.render(
     <App/>,
